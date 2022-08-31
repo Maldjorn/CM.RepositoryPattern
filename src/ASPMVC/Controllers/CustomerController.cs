307da77487082630aa907aba6bc72b298693ab51
Ivan Timoschenko <timoschenko.ivan5@gmail.com>
2022-08-31 09:32:36 +0300

    feat: add mvc

---
+++ src/ASPMVC/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using CM.Customers;
+using CM.Customers.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<Customer> customers = new List<Customer>();
+        IRepository<Customer> repository;
+        public ActionResult Index()
+        {
+            repository = new CustomerRepository();
+            customers = repository.GetAll();
+            return View(customers);
+        }
+
+        public ActionResult Create()
+        {
+            repository = new CustomerRepository();
+            
+            return View();
+        }
+    }
+}
